2f74e0144bcab406a403d987d0b2e630d64a7e38
Aakash Singh <akash7500singh@gmail.com>
2022-07-31 19:11:37 +0530

    create Sleuth based Architecture

--- fraud/src/main/java/com/akash/FraudCheckService.java
+++ fraud/src/main/java/com/akash/FraudCheckService.java
@@ -1,10 +1,12 @@
 package com.akash;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.test.web.client.UnorderedRequestExpectationManager;
 
 import com.amigoscode.clients.fraud.FraudCheckException;
 
